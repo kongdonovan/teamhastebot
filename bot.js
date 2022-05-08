0fa0c99392a7e6669f2bd5116597cba69e3ae450
Donovan's Personal Dev Space <donovanspersonaldevspace@Donovans-MacBook-Air.local>
2022-05-07 17:39:17 -0700

    wrapped activities into objects

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const fs = require('node:fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS], presence: {status: "dnd", activities: ["over the people of Team Haste", "WATCHING"]} });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS], presence: {status: "dnd", activities: [{name: "over the people of Team Haste"}, {type: "WATCHING"}]} });
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
