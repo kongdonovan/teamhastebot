58b5da587396e217d4884ef0c83bf20eb0b24a7d
Donovan's Personal Dev Space <donovanspersonaldevspace@Donovans-MacBook-Air.local>
2022-05-07 17:46:06 -0700

    messing with activities

--- bot.js
+++ bot.js
@@ -1,9 +1,8 @@
 const fs = require('node:fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
-const { ActivityTypes } = require('discord.js/typings/enums');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS], presence: {status: "dnd", activities: [{name: "over the people of Team Haste"}, {type: ActivityTypes.WATCHING}]} });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS], presence: {status: "dnd", activities: [{name: "over the people of Team Haste"}, {type: 3}]} });
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
