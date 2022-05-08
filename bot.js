f710f29d13be56d20d0790fc681344e64fe4a9ed
Donovan's Personal Dev Space <donovanspersonaldevspace@Donovans-MacBook-Air.local>
2022-05-07 19:19:18 -0700

    added flags

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const fs = require('node:fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS], presence: {status: "dnd", activities: [{name: "over Team Haste", type: 3}]} });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES], presence: {status: "dnd", activities: [{name: "over Team Haste", type: 3}]} });
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
