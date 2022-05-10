43cf817f5933fb783260e313a9a1edb00032e31e
Donovan's Personal Dev Space <donovanspersonaldevspace@Donovans-MacBook-Air.local>
2022-05-09 23:10:10 -0700

    added donate command

---
+++ commands/donate.js
@@ -0,0 +1,20 @@
+/**
+ * Displays Donovan's venmo
+ */
+
+const { MessageEmbed } = require('discord.js');
+const { Guild } = require('discord.js');
+const { prefix } = require('../config.json');
+
+module.exports = {
+    name: "donate",
+    description: "Shows ways to donate to Donovan",
+    arglen: 0,
+    argrequired: true,
+    execute(message) {
+        const embed = new MessageEmbed()
+            .setTitle('pls donate to donovan 🥺')
+            .setDescription('you can venmo me @kongdonovan');
+        return message.channel.send({ embeds: [embed] });
+    }
+}
