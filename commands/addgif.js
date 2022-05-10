65c3242b41f4673d18b658c90ccec6523ff03c31
Donovan's Personal Dev Space <donovanspersonaldevspace@Donovans-MacBook-Air.local>
2022-05-09 22:52:28 -0700

    added addgif

---
+++ commands/addgif.js
@@ -0,0 +1,40 @@
+/**
+ * Adds gif to the specified array
+ */
+
+const { MessageEmbed } = require('discord.js');
+const { Guild } = require('discord.js');
+const { prefix } = require('../config.json');
+const fs = require('fs').promises;
+
+module.exports = {
+    name: "addgif",
+    description: "Adds a gif to the specified array. Make sure to provide the link of the image source.",
+    args:"<array> <image link>",
+    arglen: 2,
+    argrequired: true,
+    async execute(message) {
+        let cmdArray = message.content.split(" ");
+        const embed = new MessageEmbed();
+        if (!message.member.permissions.has('ADMINISTRATOR')) {
+            embed.setTitle('An error has occured!').setDescription('You do not have permission to use this command.');
+        } else {
+            let dir = __dirname
+            let currentImgs = await fs.readFile(dir + '/images.json', 'utf8');
+            let imageArrays = JSON.parse(currentImgs);
+            let imageArray = imageArrays[cmdArray[1]];
+            if (!imageArray) {
+                embed.setTitle('An error has occured!').setDescription('You have provided an invalid array.');
+            } else {
+                imageArray.push(cmdArray[2]);
+                let newJson = JSON.stringify(imageArrays);
+                await fs.writeFile(dir + '/images.json', newJson);
+                embed
+                    .setTitle('Command successfully executed!')
+                    .setDescription('The following gif has been added:')
+                    .setImage(cmdArray[2]);
+            }
+        }
+        return message.channel.send({ embeds: [embed] });
+    }
+}
